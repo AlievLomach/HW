6c409177024bdb339fd70dfd8a41fceb658a9e95
AlievLomach <maks.nikolas@gmail.com>
2023-09-17 15:31:45 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import { Provider } from 'react-redux'
-import { store } from 'store/store'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import { CssBaseline } from '@mui/material'
 
 const theme = createTheme({
     palette: {
@@ -14,13 +9,4 @@ const theme = createTheme({
     },
 })
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <BrowserRouter basename="practices_react">
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <App />
-            </ThemeProvider>
-        </BrowserRouter>
-    </Provider>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
